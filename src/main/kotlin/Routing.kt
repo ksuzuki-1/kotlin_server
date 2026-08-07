9daec4149e4b8d94a8e88ca3d8eed0c98e395b30
ksuzuki <ksuzuki5145@gmail.com>
2026-08-07 23:42:49 +0900

    新規エンドポンとの追加
    - htmlコンテンツを返す/home
    
    - クエリパラメータを処理する/eat
    
    - リソースファイルからhtmlを返す/content
    
    各エンドポイントのテストを追加

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,6 +1,9 @@
 package com.ksuzuki
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.uri
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,7 +13,14 @@ fun Application.configureRouting() {
             call.respondText("Hello, World!")
         }
         get("/home") {
-            call.respondText("Home page!")
+            val text = "<h1>Ktor Server</h1>"
+            call.respondText(text, ContentType.parse("text/html"))
         }
+        get("/eat") {
+            val param = call.request.queryParameters["food"]
+            call.respondText { "Eat $param" }
+        }
+
+        staticResources("/content", "mycontent")
     }
 }
--- src/test/kotlin/ServerTest.kt
+++ src/test/kotlin/ServerTest.kt
@@ -1,7 +1,9 @@
 package com.ksuzuki
 
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.server.testing.testApplication
 import kotlin.test.*
 
@@ -9,10 +11,46 @@ class ServerTest {
 
     @Test
     fun `test root endpoint`() = testApplication {
-        // loads default configuration
         configure()
+
         // verify server root returns 200
         assertEquals(HttpStatusCode.OK, client.get("/").status)
     }
 
+    @Test
+    fun `test home endpoint`() = testApplication {
+        configure()
+
+        val response = client.get("/home")
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Ktor Server")
+    }
+
+    @Test
+    fun `test eat endpoint`() = testApplication {
+        configure()
+
+        val response = client.get("/eat?food=eel")
+        assertEquals("Eat eel", response.bodyAsText())
+    }
+
+    @Test
+    fun `test content endpoint`() = testApplication {
+        configure()
+
+        val response = client.get("/content/sample.html")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "This page is built with:")
+    }
+
+    @Test
+    fun `test missing content returns 404`() = testApplication {
+        configure()
+
+        val response = client.get("/content/not-found.html")
+
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
 }
